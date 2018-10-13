be154e6aa3bd9722ff30740008631ce2a61c6403
Jangchulwoon <lusiue@naver.com>
2018-10-13 14:27:12 +0900

    [refactoring] step01 extract method

--- src/test/java/org/toy/til/refactoring/ch01/Customer.groovy
+++ src/test/java/org/toy/til/refactoring/ch01/Customer.groovy
@@ -19,27 +19,10 @@ class Customer {
         String result = getName() + "고객님의 대여 기록 \n"
 
         while (rentals.hasMoreElements()) {
-            double thisAmount = 0;
             Rental each = (Rental) rentals.nextElement()
 
             // 비디오 종류별 대여료 계산
-            switch (each.getMovie().getPriceCode()) {
-                case Movie.REGULAR:
-                    thisAmount += 2;
-                    if (each.getDaysRented() > 2) {
-                        thisAmount += (each.getDaysRented() - 2) * 15
-                    }
-                    break
-                case Movie.NEW_RELEASE:
-                    thisAmount += each.getDaysRented() * 3
-                    break
-                case Movie.CHILD_RENS:
-                    thisAmount += 1.5
-                    if (each.getDaysRented() > 3) {
-                        thisAmount += (each.getDaysRented() - 3) * 1.5
-                    }
-                    break;
-            }
+            double thisAmount = amountFor(each)
             // 적립 포인트 1 증가
             frequentRenterPoints++
 
@@ -55,6 +38,28 @@ class Customer {
             // 현재까지 누적된 총 대여료
             totalAmount += thisAmount
         }
+    }
+
+    private double amountFor(Rental each) {
+        double thisAmount = 0
+        switch (each.getMovie().getPriceCode()) {
+            case Movie.REGULAR:
+                thisAmount += 2
+                if (each.getDaysRented() > 2) {
+                    thisAmount += (each.getDaysRented() - 2) * 15
+                }
+                break
+            case Movie.NEW_RELEASE:
+                thisAmount += each.getDaysRented() * 3
+                break
+            case Movie.CHILD_RENS:
+                thisAmount += 1.5
+                if (each.getDaysRented() > 3) {
+                    thisAmount += (each.getDaysRented() - 3) * 1.5
+                }
+                break;
+        }
+        return thisAmount
     }
 }
 
