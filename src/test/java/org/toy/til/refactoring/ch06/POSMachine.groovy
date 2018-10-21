842da14627cfe080b3cddd86b20db4b410cdffff
Jangchulwoon <lusiue@naver.com>
2018-10-21 11:47:06 +0900

    [refactoring] extract method

--- src/test/java/org/toy/til/refactoring/ch06/POSMachine.groovy
+++ src/test/java/org/toy/til/refactoring/ch06/POSMachine.groovy
@@ -2,21 +2,35 @@ package org.toy.til.refactoring.ch06
 
 class POSMachine {
 
-    void printOwing(List<Order> orderList) {
+    void printOwing(orderList) {
 
+        printBanner()
+
+        def totalPrice = getTotalPrice(orderList)
+
+        printTotalPrice(totalPrice)
+    }
+
+    // 지역변수가 단순히 읽히기만 한다면, 넘기면 된다. (java 의 경우 final 명시해주면 좋을 듯 )
+    private void printTotalPrice(totalPrice) {
+        println "---- ---- ---- -----"
+        println "TOTAL PRICE : ${totalPrice}"
+        println "---- ---- ---- -----"
+    }
+
+    // 지역변수가 외부에서도 쓰일 경우, return 하는 형식으로 사용. totalPrice를 매개변수로 받는건 좋지 못함.
+    private int getTotalPrice(orderList) {
         def totalPrice = 0
-
-        println("---- ---- ---- -----")
-        println("---- order List ----")
-        println("---- ---- ---- -----")
-
         for (Order order : orderList) {
             totalPrice += order.price
         }
+        totalPrice
+    }
 
-        println("---- ---- ---- -----")
-        println("TOTAL PRICE : ${totalPrice}")
-        println("---- ---- ---- -----")
+    private void printBanner() {
+        println "---- ---- ---- -----"
+        println "---- order List ----"
+        println "---- ---- ---- -----"
     }
 
 }
