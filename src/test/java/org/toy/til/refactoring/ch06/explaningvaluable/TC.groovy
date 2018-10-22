0c44fcc839b7a11ded754663ecd8202628739f67
Jangchulwoon <lusiue@naver.com>
2018-10-22 11:49:56 +0900

    [refactoring] explaing valuable EX-2

--- src/test/java/org/toy/til/refactoring/ch06/explaningvaluable/TC.groovy
+++ src/test/java/org/toy/til/refactoring/ch06/explaningvaluable/TC.groovy
@@ -4,9 +4,30 @@ import spock.lang.Specification
 
 class TC extends Specification {
 
-
+    // using explaining variable
     def getPrice(double quantity, double itemPrice) {
-        return quantity * itemPrice - Math.max(0, quantity - 500) * itemPrice * 0.05 + Math.min(quantity * itemPrice * 0.1, 100.0)
+        def basePrice = quantity * itemPrice
+        def discountPrice = Math.max(0, quantity - 500) * itemPrice * 0.05
+        def shippingPrice = Math.min(basePrice * 0.1, 100.0)
+        return basePrice - discountPrice + shippingPrice
+    }
+
+    // using extract method !
+    // this can approach from the other method of object.
+    def getPrice_extractMethod(double quantity, double itemPrice) {
+        return getBasePrice(quantity, itemPrice) - getDiscountPrice(quantity, itemPrice) + getShippingPrice(quantity, itemPrice)
+    }
+
+    private double getShippingPrice(double quantity, double itemPrice) {
+        Math.min(getBasePrice(quantity, itemPrice) * 0.1, 100.0)
+    }
+
+    private double getDiscountPrice(double quantity, double itemPrice) {
+        Math.max(0, quantity - 500) * itemPrice * 0.05
+    }
+
+    private double getBasePrice(double quantity, double itemPrice) {
+        quantity * itemPrice
     }
 
 
@@ -14,6 +35,7 @@ class TC extends Specification {
         expect:
         verifyAll {
             getPrice(quantity, itemPrice) == price
+            getPrice_extractMethod(quantity, itemPrice) == price
         }
         where:
         quantity << [10.0, 50.0]
